688c88cef936a6cdd098270618f3801b198d14e6
Amy Funderburg <afunderburg@live.com>
2021-05-11 15:46:19 -0700

    Added website code and images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <section className="item1">
+          <h1>Learn to code by watching others</h1>
+          <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
+            but understanding how developers think is invaluable.</p>
+        </section>
+        <section className="item2">
+          <div>
+            <button type="submit"><span>Try it free 7 days</span> then $20/mo. thereafter</button>
+          </div>
+          <form>
+            <input type="text" name="firstname" placeholder="First Name"/><br/>
+              <input type="text" name="lastname" placeholder="Last Name"/><br/>
+                <input type="email" name="email" placeholder="Email Address"/><br/>
+                  <input type="password" name="password" placeholder="Password"/><br/>
+                    <button type="submit">CLAIM YOUR FREE TRIAL</button>
+                    <p>By clicking the button, you are agreeing to our <span>Terms and Services</span></p>
+          </form>
+        </section>
+      </div>
     </div>
   );
 }
